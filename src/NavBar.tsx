d6c4f7b623ed42c481c563f8df93c1f6040ab29a
Debanjum Singh Solanky <debanjum@gmail.com>
2023-05-31 13:22:57 +0530

    Remove central waves and square nav buttons on top pane

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -17,10 +17,10 @@ export default function NavBar() {
                 <div className="navContent">
                     <ul className="navLinks">
                         <li className="navLinks">
-                            <Button type="primary" shape="default" size="large">
+                            <Button type="primary" shape="default" style={{borderRadius: '4px'}}>
                                 <Link className="navLinks" to="https://github.com/debanjum/khoj">Install</Link>
                             </Button>
-                            <Button type="text" shape="default" size="large" href="/about">
+                            <Button type="text" shape="default" href="/about" style={{borderRadius: '4px'}}>
                                 About
                             </Button>
                         </li>
