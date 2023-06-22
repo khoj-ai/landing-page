e00ae5bd02642649a899f2bc17ae3f90a85f938f
sabaimran <narmiabas@gmail.com>
2023-06-21 19:05:03 -0700

    Update the layout of the landing page to support a Menu and centralize the header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import NavBar from './NavBar';
 import './styles/App.css';
 import Home from './pages/Home';
 import About from './pages/About';
-import Waitlist from './components/Waitlist';
+import Invited from './pages/Invited';
+import Login from './pages/Login';
 
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, Layout } from 'antd';
+
+const { Content } = Layout;
 
 import {
 	BrowserRouter,
@@ -25,21 +28,28 @@ function App() {
 						}
 					}
 				}>
-
 				<BrowserRouter>
-					<NavBar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/waitlist" element={<Waitlist />} />
-					</Routes>
+					<Layout>
+						<Layout style={{ padding: 0, background: "#fff" }}>
+							<NavBar />
+							<Content>
+								<Routes>
+									<Route path="/" element={<Home />} />
+									<Route path="/about" element={<About />} />
+									<Route path="/invited/:inviteId" element={<Invited />} />
+									<Route path="/login" element={<Login />} />
+								</Routes>
+							</Content>
+							<footer className="footer-container">
+								<img src='https://raw.githubusercontent.com/khoj-ai/landing-page/master/public/footer_garden.svg' className='footer-garden' alt='footer-garden' />
+								<p className='footer-text'>
+									Designed with ❤️ by Debankon.<br />Developed with 🥵 by Saba and Debanjum.
+								</p>
+							</footer>
+						</Layout>
+					</Layout>
 				</BrowserRouter>
-				<footer className="footer-container">
-					<img src='https://raw.githubusercontent.com/khoj-ai/landing-page/master/public/footer_garden.svg' className='footer-garden' alt='footer-garden' />
-					<p className='footer-text'>
-						Designed with ❤️ by Debankon.<br />Developed with 🥵 by Saba and Debanjum.
-					</p>
-				</footer>
+
 			</ConfigProvider>
 		</div>
   )	;
