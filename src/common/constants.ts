9c3ad497dfe8b6e59f24affdecd02607723a6ad7
sabaimran <narmiabas@gmail.com>
2023-06-30 11:33:17 -0700

    Update Discord link again. It keeps expiring.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,3 @@
 export const APIURL = process.env.NODE_ENV == 'production' ? "https://lantern.khoj.dev" : 'http://localhost:5000';
-export const DISCORD_LINK = "https://discord.gg/fP89zSJb";
+export const DISCORD_LINK = "https://discord.gg/BDgyabRM6e";
 export const KHOJ_LINK = process.env.NODE_ENV == 'production' ? "https://lantern.khoj.dev" : 'http://localhost:5000';
